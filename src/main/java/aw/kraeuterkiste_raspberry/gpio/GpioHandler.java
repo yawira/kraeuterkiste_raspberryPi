883078f769af012e61b2160627fe8e85c0bf6c86
Lorenz Josten <lorenz.josten@gmail.com>
2019-08-27 14:45:40 +0200

    fix GpioHandler

--- src/main/java/aw/kraeuterkiste_raspberry/gpio/GpioHandler.java
+++ src/main/java/aw/kraeuterkiste_raspberry/gpio/GpioHandler.java
@@ -4,9 +4,6 @@ import com.pi4j.io.gpio.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 @Component
 public class GpioHandler {
 
@@ -22,36 +19,68 @@ public class GpioHandler {
 
     private GpioController gpioController;
 
-    @PostConstruct
-    public void initGpioController() {
+//    @PostConstruct
+//    public void initGpioController() {
+//        gpioController = GpioFactory.getInstance();
+//        ledPin = RaspiPin.getPinByName(ledPinName);
+//        pumpPin = RaspiPin.getPinByName(pumpPinName);
+//        ledOutputPin = gpioController.provisionDigitalOutputPin(ledPin);
+//        pumpOutputPin = gpioController.provisionDigitalOutputPin(pumpPin);
+//    }
+//
+//    @PreDestroy
+//    public void closeGpioController() {
+//        gpioController.unprovisionPin(ledOutputPin);
+//        gpioController.unprovisionPin(pumpOutputPin);
+//        gpioController.shutdown();
+//    }
+
+    public void toggleLight() {
         gpioController = GpioFactory.getInstance();
-        ledPin = RaspiPin.getPinByName(ledPinName);
-        pumpPin = RaspiPin.getPinByName(pumpPinName);
+        ledPin = RaspiPin.GPIO_04;
         ledOutputPin = gpioController.provisionDigitalOutputPin(ledPin);
-        pumpOutputPin = gpioController.provisionDigitalOutputPin(pumpPin);
+
+        ledOutputPin.toggle();
+
+        gpioController.unprovisionPin(ledOutputPin);
+        gpioController.shutdown();
     }
 
-    @PreDestroy
-    public void closeGpioController() {
+    public boolean isLedOn() {
+        gpioController = GpioFactory.getInstance();
+        ledPin = RaspiPin.GPIO_04;
+        ledOutputPin = gpioController.provisionDigitalOutputPin(ledPin);
+
+        boolean ledOn = ledOutputPin.getState() != PinState.HIGH;
+
         gpioController.unprovisionPin(ledOutputPin);
+        gpioController.shutdown();
+
+        return ledOn;
+    }
+
+    public void togglePump() {
+        gpioController = GpioFactory.getInstance();
+        pumpPin = RaspiPin.GPIO_05;
+        pumpOutputPin = gpioController.provisionDigitalOutputPin(pumpPin);
+
+        pumpOutputPin.toggle();
+
         gpioController.unprovisionPin(pumpOutputPin);
         gpioController.shutdown();
     }
 
-    public void toggleLight() {
-        ledOutputPin.toggle();
-    }
-
-    public boolean isLedOn() {
-        return ledOutputPin.getState() != PinState.HIGH;
-    }
-
-    public void togglePump() {
-        pumpOutputPin.toggle();
-    }
-
     public boolean isPumpOn() {
-        return pumpOutputPin.getState() != PinState.HIGH;
+        gpioController = GpioFactory.getInstance();
+        pumpPin = RaspiPin.GPIO_05;
+        pumpOutputPin = gpioController.provisionDigitalOutputPin(pumpPin);
+
+        boolean pumpOn = pumpOutputPin.getState() != PinState.HIGH;
+
+        gpioController.unprovisionPin(pumpOutputPin);
+        gpioController.shutdown();
+
+        return pumpOn;
     }
 
 }
