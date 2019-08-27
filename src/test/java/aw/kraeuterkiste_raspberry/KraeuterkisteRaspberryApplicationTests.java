efb7a1ad6964e1dda2c013c84e0d159c01bcb793
Lorenz Josten <lorenz.josten@gmail.com>
2019-08-27 13:59:18 +0200

    add mock to test in order to make packaging test pass change gitignore behaviour

--- src/test/java/aw/kraeuterkiste_raspberry/KraeuterkisteRaspberryApplicationTests.java
+++ src/test/java/aw/kraeuterkiste_raspberry/KraeuterkisteRaspberryApplicationTests.java
@@ -1,13 +1,18 @@
 package aw.kraeuterkiste_raspberry;
 
+import aw.kraeuterkiste_raspberry.gpio.GpioHandler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KraeuterkisteRaspberryApplicationTests {
+
+    @MockBean
+    GpioHandler gpioHandler;
 
     @Test
     public void contextLoads() {
